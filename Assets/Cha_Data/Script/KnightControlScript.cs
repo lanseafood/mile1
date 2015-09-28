28e43fda6c7d41ada5dbcfa1577d2787570dfec1
lanseafood <l.mao.ftwin@gmail.com>
2015-09-28 17:17:52 -0400

    First Commit
    Everything

---
+++ Assets/Cha_Data/Script/KnightControlScript.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Animator))]
+[RequireComponent(typeof (CapsuleCollider))]
+[RequireComponent(typeof (Rigidbody))]
+
+public class KnightControlScript : MonoBehaviour {
+
+	[System.NonSerialized]					
+	public float lookWeight;					// the amount to transition when using head look
+	
+	[System.NonSerialized]
+	public Transform enemy;						// a transform to Lerp the camera to during head look
+	
+	public float animSpeed = 1.5f;				// a public setting for overall animator animation speed
+	public float lookSmoother = 3f;				// a smoothing setting for camera motion
+	public bool useCurves;
+	//public float Gravity = 20f;
+
+	private Animator anim;							// a reference to the animator on the character
+	private AnimatorStateInfo currentBaseState;			// a reference to the current state of the animator, used for base layer
+	private AnimatorStateInfo layer2CurrentState;	// a reference to the current state of the animator, used for layer 2
+	private CapsuleCollider col;					// a reference to the capsule collider of the character
+	//private float gravity;
+	private CharacterController charCtrl;
+
+	static int idleState = Animator.StringToHash("Base Layer.Idle");	
+	static int locoState = Animator.StringToHash("Base Layer.Locomotion");			// these integers are references to our animator's states
+	static int jumpState = Animator.StringToHash("Base Layer.Jump");				// and are used to check state for various actions to occur
+
+	// Use this for initialization
+	void Start () {
+		// initialising reference variables
+		anim = GetComponent<Animator>();					  
+		col = GetComponent<CapsuleCollider>();				
+		//enemy = GameObject.Find("Enemy").transform;	
+		//if(anim.layerCount ==2)
+		//	anim.SetLayerWeight(1, 1);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+		float v = Input.GetAxis("Vertical");				// setup v variables as our vertical input axis
+		anim.SetFloat("Speed", v);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		anim.SetFloat("Direction", h); 						// set our animator's float parameter 'Direction' equal to the horizontal input axis		
+		anim.speed = animSpeed;								// set the speed of our animator to the public variable 'animSpeed'
+		anim.SetLookAtWeight(lookWeight);					// set the Look At Weight - amount to use look at IK vs using the head's animation
+		currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// set our currentState variable to the current state of the Base Layer (0) of animation
+		
+		//if(anim.layerCount ==2)		
+		//	layer2CurrentState = anim.GetCurrentAnimatorStateInfo(1);	// set our layer2CurrentState variable to the current state of the second Layer (1) of animation
+		//Physics.gravity = Vector3(0, -1.0, 0);
+		//charCtrl.SimpleMove(gameObject.transform.up * Gravity * Time.deltaTime);
+
+		if (currentBaseState.nameHash == locoState)
+		{
+			if(Input.GetButtonDown("Jump"))
+			{
+				anim.SetTrigger("Jump");
+			}
+
+		}
+//		else if(currentBaseState.nameHash == jumpState)
+//		{
+//			//  ..and not still in transition..
+//			if(!anim.IsInTransition(0))
+//			{
+//				if(useCurves)
+//					// ..set the collider height to a float curve in the clip called ColliderHeight
+//					col.height = anim.GetFloat("ColliderHeight");
+//				
+//				// reset the Jump bool so we can jump again, and so that the state does not loop 
+//				anim.SetBool("Jump", false);
+//			}
+//			
+//			// Raycast down from the center of the character.. 
+//			Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
+//			RaycastHit hitInfo = new RaycastHit();
+//			
+//			if (Physics.Raycast(ray, out hitInfo))
+//			{
+//				// ..if distance to the ground is more than 1.75, use Match Target
+//				if (hitInfo.distance > 1.75f)
+//				{
+//					
+//					// MatchTarget allows us to take over animation and smoothly transition our character towards a location - the hit point from the ray.
+//					// Here we're telling the Root of the character to only be influenced on the Y axis (MatchTargetWeightMask) and only occur between 0.35 and 0.5
+//					// of the timeline of our animation clip
+//					anim.MatchTarget(hitInfo.point, Quaternion.identity, AvatarTarget.Root, new MatchTargetWeightMask(new Vector3(0, 1, 0), 0), 0.35f, 0.5f);
+//				}
+//			}
+//		}
+	}
+}
